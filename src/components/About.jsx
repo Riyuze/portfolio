a584e26edabdf8a0400e5159428856bd8e65c3a8
Kevin Tandavo <riyuze211001@gmail.com>
2022-07-01 19:30:25 +0700

    Fixed a Few Bugs

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,11 +8,11 @@ function About() {
           <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
         </div>
           <p className='text-xl mt-20'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
           </p>
           <br></br>
           <p className='text-xl'>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, se
           </p>
       </div>
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
             </div>
 
             <div>
-                <img src={HeroImage} alt="my profile" className='rounded-2xl mx-auto w-2/3'></img>
+                <img src={HeroImage} alt="my profile" className='rounded-2xl mx-auto w-2/3 my-5'></img>
             </div>
 
         </div>
