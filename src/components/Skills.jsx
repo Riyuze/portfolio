4d36715c24b808844c51765e5898a98449a9043e
Kevin Tandavo <riyuze211001@gmail.com>
2022-07-01 18:14:55 +0700

    Added Skills Images

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Arduino from '../assets/skills/arduino.png';
+import Bootstrap from '../assets/skills/bootstrap.png';
+import CPlusPlus from '../assets/skills/cplusplus.png';
+import CSS from '../assets/skills/css.png';
+import Django from '../assets/skills/django.png';
+import Git from '../assets/skills/git.png';
+import Heroku from '../assets/skills/heroku.png';
+import HTML from '../assets/skills/html.png';
+import Javascript from '../assets/skills/javascript.png';
+import Python from '../assets/skills/python.png';
+import ReactLogo from '../assets/skills/react.png';
+import SQL from '../assets/skills/sql.png';
+import Tailwind from '../assets/skills/tailwind.png';
+import Tensorflow from '../assets/skills/tensorflow.png';
+
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <div>
+
+    </div>
   )
 }
 
