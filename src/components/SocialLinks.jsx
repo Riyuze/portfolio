4f50cc9e4cc1bba649aed415c08bbf974baa1a68
Kevin Tandavo <riyuze211001@gmail.com>
2022-08-07 03:58:53 +0700

    Updated Resume File

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -16,16 +16,16 @@ function SocialLinks() {
             id: 3, child: (<> Email <HiOutlineMail size={30}/> </>), href: 'mailto:kevo211001@gmail.com'
         },
         {
-            id: 4, child: (<> Resume <BsFillPersonLinesFill size={30}/> </>), href: 'https://drive.google.com/file/d/1lbLENzC5TBP6U5S0isSmfmwApU--Gy2M/view?usp=sharing', style: 'rounded-br-md', download: true,
+            id: 4, child: (<> Resume <BsFillPersonLinesFill size={30}/> </>), href: 'https://drive.google.com/drive/folders/1jLDYvhanYdtqzENRj16Niuvk6XJfmLWQ?usp=sharing', style: 'rounded-br-md',
         },
     ];
 
   return (
     <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
         <ul>
-            {links.map(({id, child, href, style, download}) => (
+            {links.map(({id, child, href, style}) => (
                 <li key={id} className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 ' + style}>
-                    <a href={href} className='flex justify-between items-center w-full text-white' download={download} target='_blank' rel="noreferrer">
+                    <a href={href} className='flex justify-between items-center w-full text-white' target='_blank' rel="noreferrer">
                         {child} 
                     </a>
                 </li>
