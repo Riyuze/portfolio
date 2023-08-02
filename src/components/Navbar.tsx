7d3d0188c456d96513577b4b94fc02a0f2f50f45
Kevin Tandavo <riyuze211001@gmail.com>
2023-08-03 01:13:40 +0700

    Bug fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -57,7 +57,7 @@ const Navbar = () => {
                     <div
                         className={`${
                             !toggle ? "hidden" : "flex"
-                        } p-6 absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+                        } bg-primary p-6 absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
                     >
                         <ul className="list-none flex justify-end items-start flex-col gap-4">
                             {navLinks.map((link) => {
